1fc108eb1b904d5af5c70db0dc4a498046a3c792
Ludaro <waschmaschvanlu@gmail.com>
2023-10-18 18:11:12 +0200

    Update callbacks.lua

--- server/callbacks.lua
+++ server/callbacks.lua
@@ -4,5 +4,9 @@ end)
 
 
 lib.callback.register('ludaro_container:hasItem', function(source)
-    return ESX.GetPlayerFromId(source).hasItem(Config.ItemName, 1).count > 0 or false
+if Config.NeedsItem then
+return ESX.GetPlayerFromId(source).hasItem(Config.ItemName, 1).count > 0 or false
+        else
+return false
+        end
 end)
